00b93306a62057af3177051718363fb6e53b1293
Sandro Simas <sandro.csimas@gmail.com>
2018-07-30 01:16:20 -0300

    Changing test environment website url

--- client/configs/settings.js
+++ client/configs/settings.js
@@ -9,8 +9,8 @@ const config = yaml.load(path.resolve('client', 'config.yml'));
 module.exports = (env) => {
   const settings = {env};
   if (env === 'production') {
-    settings.webUrl = 'http://ec2-13-57-249-230.us-west-1.compute.amazonaws.com:8080';
-    settings.apiUrl = 'http://ec2-13-57-249-230.us-west-1.compute.amazonaws.com:8081';
+    settings.webUrl = 'http://ec2-54-67-84-151.us-west-1.compute.amazonaws.com:8080';
+    settings.apiUrl = 'http://ec2-54-67-84-151.us-west-1.compute.amazonaws.com:8081';
   } else {
     settings.webUrl = _.get(config, 'web.url', 'http://localhost:4000');
     settings.apiUrl = _.get(config, 'api.url', 'http://localhost:3000');
