863a1ec7794f64ba6d1e0cea5aaa0580f5cb45fb
Sandro Simas <sandro.csimas@gmail.com>
2018-07-31 22:06:35 -0300

    Updating webUrl and apiUrl in server settings

--- client/configs/settings.js
+++ client/configs/settings.js
@@ -9,8 +9,8 @@ const config = yaml.load(path.resolve('client', 'config.yml'));
 module.exports = (env) => {
   const settings = {env};
   if (env === 'production') {
-    settings.webUrl = 'http://ec2-54-67-84-151.us-west-1.compute.amazonaws.com:8080';
-    settings.apiUrl = 'http://ec2-54-67-84-151.us-west-1.compute.amazonaws.com:8081';
+    settings.webUrl = 'https://www.intercamb.me';
+    settings.apiUrl = 'https://api.intercamb.me';
   } else {
     settings.webUrl = _.get(config, 'web.url', 'http://localhost:4000');
     settings.apiUrl = _.get(config, 'api.url', 'http://localhost:3000');
