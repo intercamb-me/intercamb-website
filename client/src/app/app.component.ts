e07cb7849965b7e8cbb16932e0d8bab69bb31091
Sandro Simas <sandro.csimas@gmail.com>
2018-08-23 01:12:31 -0300

    Preventing smooch to be initialized in client form view

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 import * as Chart from 'chart.js';
 import * as Smooch from 'smooch';
 
@@ -8,21 +9,35 @@ import * as Smooch from 'smooch';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {
+  private smoochInitialized: boolean;
+
+  constructor(private router: Router) {
 
   }
 
   public ngOnInit(): void {
     Chart.defaults.global.defaultFontFamily = 'Montserrat';
-    Smooch.init({
-      appId: '5985c40908d768ef00cbb4f1',
-      locale: 'pt-BR',
-      customText: {
-        headerText: 'Como posso te ajudar?',
-        inputPlaceholder: 'Digite uma mensagem...',
-        sendButtonText: 'Enviar',
-        introductionText: 'Estamos aqui para conversar. Sinta-se a vontade para perguntar qualquer coisa.',
-      },
+    this.router.events.subscribe((event) => {
+      this.onRouteChanged(event);
     });
+  }
+
+  private onRouteChanged(event?: any): void {
+    if (event && event instanceof NavigationEnd) {
+      window.scroll(0, 0);
+      if (!this.smoochInitialized && !event.url.startsWith('/clients/form')) {
+        this.smoochInitialized = true;
+        Smooch.init({
+          appId: '5985c40908d768ef00cbb4f1',
+          locale: 'pt-BR',
+          customText: {
+            headerText: 'Como posso te ajudar?',
+            inputPlaceholder: 'Digite uma mensagem...',
+            sendButtonText: 'Enviar',
+            introductionText: 'Estamos aqui para conversar. Sinta-se a vontade para perguntar qualquer coisa.',
+          },
+        });
+      }
+    }
   }
 }
