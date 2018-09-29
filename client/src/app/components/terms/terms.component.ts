3a823052f50c426f96f467aee8340261194360d6
Sandro Simas <sandro.csimas@gmail.com>
2018-09-29 01:32:19 -0300

    Finishing terms of use; Adding temporary favicon;

---
+++ client/src/app/components/terms/terms.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-terms',
+  templateUrl: './terms.component.html',
+})
+export class TermsComponent {
+
+  constructor() {
+
+  }
+}
